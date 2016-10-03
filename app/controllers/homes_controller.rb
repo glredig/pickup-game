0c42c6391cebc12512e9e888c14c2262111e6718
Gabriel Redig <glredig@gmail.com>
2016-10-02 23:50:13 -0700

    fix dates in controller

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
 	def index
-		@next_game = Game.where(date: Date.today..1.week.from_now).first
+		@next_game = Game.where(date: Date.today.in_time_zone("Pacific Time (US & Canada)")..1.week.from_now).first
 	end
 end
