5e3b287657ce3aae749f4bbc8165f811faeecf6e
Gabriel Redig <glredig@gmail.com>
2016-10-25 00:23:41 -0700

    change how next game is set

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
 	def index
-		@next_game = Game.where('date > ?', DateTime.yesterday ).first
+		@next_game = Game.where('date > ?', DateTime.now.beginning_of_day ).first
 	end
 end
